6241bbe4e5edad4618195f5a10ed0b3b3755e09c
GustavJones <gustavjones3@gmail.com>
2024-09-26 00:19:32 +0200

    Add option for static building

--- include/GParsing/Core/API.hpp
+++ include/GParsing/Core/API.hpp
@@ -3,17 +3,29 @@
 #if defined(_MSC_VER)
 #undef GPARSING_API
 
+#ifdef GPARSING_SHARED
+
 #ifdef GPARSING_BUILD_DLL
 #define GPARSING_API __declspec(dllexport)
 #else
 #define GPARSING_API __declspec(dllimport)
 #endif
 
+#else
+#define GPARSING_API
+#endif
+
 #elif defined(__GNUC__)
 #undef GPARSING_API
 
+#ifdef GPARSING_SHARED
+
 #ifdef GPARSING_BUILD_DLL
 #define GPARSING_API __attribute__((visibility("default")))
+#else
+#define GPARSING_API
+#endif
+
 #else
 #define GPARSING_API
 #endif
